2bb447377181a43ef635c949d8a3bc97601766bf
zasamy <zainebzahiri@gmail.com>
2023-05-28 16:33:46 -0700

    modification code

--- print_function.c
+++ print_function.c
@@ -46,8 +46,8 @@ int print_string(va_list ap, params_t *params)
 {
 	char *str = va_arg(ap, char *), pad_char = ' ';
 	unsigned int pad = 0, sum = 0, i = 0, j;
+	
 	(void)params;
-
 	switch ((int)(!str))
 	case 1:
 		str = NULL_STRING;
--- printf.c
+++ printf.c
@@ -9,7 +9,6 @@ int _printf(const char *format, ...)
 	int sum = 0;
 	va_list ap;
 	char *p, *start;
-
 	params_t params = PARAMS_INIT;
 
 	va_start(ap, format);
