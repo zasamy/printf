a8ef9b40acb6ac94c1ed8aded9b267e3205b4966
zasamy <zainebzahiri@gmail.com>
2023-05-27 17:42:15 -0700

    aa

--- printf.c
+++ printf.c
@@ -6,38 +6,41 @@
  */
 int _printf(const char *format, ...)
 {
-int sum = 0;
-va_list ap;
-char *p, *start;
-params_t params = PARAMS_INIT;
-va_start(ap, format);
-if (format || (format[0] == '%' && !format[1]))
-return (-1);
-for (p = (char *)format; *p; p++)
-{
-init_params(&params, ap);
-if (*p != '%')
-{
-sum = sum + _putchar(*p);
-continue;
-}
-start = p;
-p++;
-while (get_flag(p, &params))
-{
-p++;
-}
-p = get_width(p, &params, ap);
-p = get_precision(p, &params, ap);
-if (get_modifier(p, &params))
-p++;
-if (!get_specifier(p))
-sum = sum + print_from_to(start, p,
-params.l_modifier || params.h_modifier ? p - 1 : 0);
-else
-sum = sum + get_print_func(p, ap, &params);
-}
-_putchar(BUF_FLUSH);
-va_end(ap);
-return (sum);
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+
+	params_t params = PARAMS_INIT;
+
+	va_start(ap, format);
+
+	if (format || (format[0] == '%' && !format[1]))
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(&params, ap);
+		if (*p != '%')
+		{
+			sum = sum + _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
+		while (get_flag(p, &params))
+		{
+			p++;
+		}
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum = sum + print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum = sum + get_print_func(p, ap, &params);
+	}
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
