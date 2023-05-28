3d1b2d4a5712a36ad59ce71cdfe4314f98b4cb7a
zasamy <zainebzahiri@gmail.com>
2023-05-27 17:32:53 -0700

    specifiers.c modif

--- specifiers.c
+++ specifiers.c
@@ -7,37 +7,38 @@
  */
 int (*get_specifier(char *s))(va_list ap, params_t *params)
 {
-specifier_t specifiers[] = {
-{"d", print_int},
-{"c", print_char},
-{"s", print_string},
-{"i", print_int},
-{"%", print_percent},
-{"b", print_binary},
-{"o", print_octal},
-{"u", print_unsigned},
-{"x", print_hex},
-{"X", print_HEX},
-{"p", print_address},
-{"S", print_S},
-{"r", print_rev},
-{"R", print_rot13},
-{NULL, NULL}
-};
-int i = 0;
-while (specifiers[i].specifier)
-{
-if (*s == specifiers[i].specifier[0])
-{
-return (specifiers[i].f);
-}
-i++;
-{
-return (specifiers[i].f);
-}
-i++;
-}
-return (NULL);
+	specifier_t specifiers[] = {
+		{"d", print_int},
+		{"c", print_char},
+		{"s", print_string},
+		{"i", print_int},
+		{"%", print_percent},
+		{"b", print_binary},
+		{"o", print_octal},
+		{"u", print_unsigned},
+		{"x", print_hex},
+		{"X", print_HEX},
+		{"p", print_address},
+		{"S", print_S},
+		{"r", print_rev},
+		{"R", print_rot13},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (specifiers[i].specifier)
+	{
+		if (*s == specifiers[i].specifier[0])
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+		{
+			return (specifiers[i].f);
+		}
+		i++;
+	}
+	return (NULL);
 }
 /**
  * get_print_func - finds the format func
@@ -49,10 +50,11 @@ return (NULL);
  */
 int get_print_func(char *s, va_list ap, params_t *params)
 {
-int (*f)(va_list, params_t *) = get_specifier(s);
-if (f)
-return (f(ap, params));
-return (0);
+	int (*f)(va_list, params_t *) = get_specifier(s);
+
+	if (f)
+		return (f(ap, params));
+	return (0);
 }
 /**
  * get_flag - finds the flag func
@@ -63,27 +65,28 @@ return (0);
  */
 int get_flag(char *s, params_t *params)
 {
-int i = 0;
-switch (*s)
-switch (*s)
-{
-case '+':
-i = params->plus_flag = 1;
-break;
-case ' ':
-i = params->space_flag = 1;
-break;
-case '#':
-i = params->hashtag_flag = 1;
-break;
-case '-':
-i = params->minus_flag = 1;
-break;
-case '0':
-i = params->zero_flag = 1;
-break;
+	int i = 0;
+
+	switch (*s)
+	{
+		case '+':
+			i = params->plus_flag = 1;
+			break;
+		case ' ':
+			i = params->space_flag = 1;
+			break;
+		case '#':
+			i = params->hashtag_flag = 1;
+			break;
+		case '-':
+			i = params->minus_flag = 1;
+			break;
+		case '0':
+			i = params->zero_flag = 1;
+			break;
+	}
+	return (i);
 }
-return (i);
 /**
  * get_modifier - find the modifierfunc
  * @s: the format of string
@@ -92,17 +95,18 @@ return (i);
  */
 int get_modifier(char *s, params_t *params)
 {
-int i = 0;
-switch (*s)
-{
-case 'h':
-i = params->h_modifier = 1;
-break;
-case 'l':
-i = params->l_modifier = 1;
-break;
-}
-return (i);
+	int i = 0;
+
+	switch (*s)
+	{
+		case 'h':
+			i = params->h_modifier = 1;
+			break;
+		case 'l':
+			i = params->l_modifier = 1;
+			break;
+	}
+	return (i);
 }
 /**
  * get_width - have the width from the format string
@@ -113,18 +117,19 @@ return (i);
  */
 char *get_width(char *s, params_t *params, va_list ap)
 {
-int d = 0;
-if (*s == '*')
-{
-d = va_arg(ap, int);
-s++;
+	int d = 0;
+
+	if (*s == '*')
+	{
+		d = va_arg(ap, int);
+		s++;
+	}
+	else
+	{
+		while (_isdigit(*s))
+			d = d * 10 + (*s++ - '0');
+	}
+		params->width = d;
+		return (s);
 }
-else
-{
-while (_isdigit(*s))
-d = d * 10 + (*s++ - '0');
-params->width = d;
-return (s);
-}
-}
-}
+
